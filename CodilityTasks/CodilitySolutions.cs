f559ddcbc7bec3c48d297b16e7e50461ca4b83af
Andy Shevchenko <AShevchenko@Flintfox.com>
2020-05-28 08:54:45 +1200

    PermMissingElem task description added

--- CodilityTasks/CodilitySolutions.cs
+++ CodilityTasks/CodilitySolutions.cs
@@ -8,6 +8,21 @@ namespace Codility
     {
         public int PermMissingElem_solution(int[] A)
         {
+            /*
+            An array A consisting of N different integers is given. 
+            The array contains integers in the range [1..(N + 1)], which means that exactly one element is missing.
+            Your goal is to find that missing element.
+            
+            Write a function: that, given an array A, returns the value of the missing element.
+            
+            For example, given array A {2, 3, 1, 5} the function should return 4, as it is the missing element.
+            
+            Write an efficient algorithm for the following assumptions:
+                N is an integer within the range [0..100,000];
+                the elements of A are all distinct;
+                each element of array A is an integer within the range [1..(N + 1)]. 
+            */
+
             int expected = 1;
             if (A.Length > 0)
             {
